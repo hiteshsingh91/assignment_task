78d164824e3f584f4db291b7951076104fd827d8
hiteshsingh <hitesh.6131@gmail.com>
2018-12-09 23:50:36 +0530

    code cleanup

--- handlers/common.js
+++ handlers/common.js
@@ -9,29 +9,5 @@ module.exports = {
     }
     return true;
   },
-
-  validateToken: async(req, res, next) => {
-    const authorizationHeaader = req.headers.authorization;
-    let result;
-    if (authorizationHeaader) {
-      const token = req.headers.authorization.split(' ')[1]; // Bearer <token>
-      const options = {
-        expiresIn: jwtTokenExpiry  
-      };
-      try {
-        result = jwt.verify(token, (process.env.JWT_SECRET || jwtSecretKey), options);
-        req.decoded = result;
-        next();
-      } catch (err) {
-        throw new Error(err);
-      }
-    } else {
-      result = { 
-        error: `Authentication error. Token required.`,
-        status: 401 
-      };
-      res.status(401).send(result);
-    }
-  }
 };
 
